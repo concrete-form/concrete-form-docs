ffd0fc3ccd11c9db5ed3be5059c3ff99c7fa4d76
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-06 14:55:59 -0400

    Updated config for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Concrete Form',
   tagline: 'The most popular React form libraries implemented for you',
-  url: 'https://concrete-form.github.io',
-  baseUrl: '/concrete-form-docs/',
+  url: 'https://concrete-form.com',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
