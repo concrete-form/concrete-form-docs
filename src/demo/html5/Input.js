b80f0c15b640a4402ab9b2fae2c856abfc723d3b
Kevin Gilbert <kevin@kegisiteweb.com>
2021-12-10 15:39:46 -0500

    Added HTML5 doc for control : Input

---
+++ src/demo/html5/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import Input from '@concrete-form/html5/Input'
+
+const Demo = () => (
+  <Form>
+    <Input name="demo" placeholder="I'm an input" />
+  </Form>
+)
+
+export default Demo
