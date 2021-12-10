87ae5a79da0ceac1538a6261deb163a4d08a522b
Kevin Gilbert <kevin@kegisiteweb.com>
2021-12-10 15:39:29 -0500

    Added HTML5 doc for control : FileInput

---
+++ src/demo/html5/FileInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import FileInput from '@concrete-form/html5/FileInput'
+
+const Demo = () => (
+  <Form>
+    <FileInput name="demo" />
+  </Form>
+)
+
+export default Demo
