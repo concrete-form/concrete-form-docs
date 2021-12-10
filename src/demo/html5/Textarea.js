8615ba3bb3d33b506abfa9e867e86334464ee183
Kevin Gilbert <kevin@kegisiteweb.com>
2021-12-10 15:41:20 -0500

    Added HTML5 doc for control : Textarea

---
+++ src/demo/html5/Textarea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import Textarea from '@concrete-form/html5/Textarea'
+
+const Demo = () => (
+  <Form>
+    <Textarea name="demo" placeholder="I'm a textarea" />
+  </Form>
+)
+
+export default Demo
