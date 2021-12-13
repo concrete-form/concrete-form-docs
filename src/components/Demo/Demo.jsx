1eeadf4b85d1062c4d1d53b939b92c1e69e11573
Kevin Gilbert <kevin@kegisiteweb.com>
2021-12-13 17:16:28 -0500

    Added a way to exclude code from demo, wip

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -3,19 +3,40 @@ import CodeBlock from '@theme/CodeBlock'
 
 import styles from './demo.module.css'
 
-const Demo = ({ Component, source }) => {
+const DELIMITER_START = '___START___'
+const DELIMITER_END = '___END___'
 
+const Demo = ({
+  Component,
+  source,
+  dense = false,
+  hideCode = false,
+}) => {
   source = source.replace(/import React from 'react'/, '')
   source = source.replace(/export default .*/, '')
   source = source.trim()
 
+  /* primitive source exclusion in demos */
+  /* todo : move this in doc utils and make it more robust */
+
+  source = source.replace(/\{? ?\/\* ?exclude\-start ?\*\/ ?\}?/g, DELIMITER_START)
+  source = source.replace(/\{? ?\/\* ?exclude\-end ?\*\/ ?\}?/g, DELIMITER_END)
+  source = source.replace(new RegExp(`${DELIMITER_START}[^__]*${DELIMITER_END}`, 'igm'), '')
+
   return (
     <>
-      <CodeBlock className={['language-jsx', styles.code].join(' ')}>
-        { source.trim() }
-      </CodeBlock>
+      {!hideCode && (
+        <CodeBlock className={['language-jsx', styles.code].join(' ')}>
+          { source.trim() }
+        </CodeBlock>
+      )}
 
-      <div className={['shadow--md', styles.result].join(' ')}>
+      <div className={[
+        'shadow--md',
+          styles.result,
+          dense ? styles.dense : '',
+          hideCode ? styles.codeHidden : '',
+        ].join(' ')}>
         <Component />
       </div>
     </>
