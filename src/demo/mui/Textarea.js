f54577bcef86613fc903471c859290284a3e4819
Kevin Gilbert <kevin@kegisiteweb.com>
2022-01-13 13:42:13 -0500

    Added MUI doc and demo for : Textarea

---
+++ src/demo/mui/Textarea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import Textarea from '@concrete-form/mui/Textarea'
+
+const Demo = () => (
+  <Form>
+    <Textarea name="demo" label="I'm a textarea" />
+  </Form>
+)
+
+export default Demo
