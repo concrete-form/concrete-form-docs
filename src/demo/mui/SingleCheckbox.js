6d7b5aa6cf47d4bb9d5b2c72fc1c7626facb7d47
Kevin Gilbert <kevin@kegisiteweb.com>
2022-01-13 13:41:43 -0500

    Added MUI doc and demo for : SingleCheckbox

---
+++ src/demo/mui/SingleCheckbox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import SingleCheckbox from '@concrete-form/mui/SingleCheckbox'
+
+const Demo = () => (
+  <Form>
+    <SingleCheckbox name="demo" label={(
+      <>I accept the <a href="#void">terms and conditions</a>.</>
+    )} />
+  </Form>
+)
+
+export default Demo
