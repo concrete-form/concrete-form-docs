b4bcfaf003a9f8a35f2b683b72c3f66911c2c963
Kevin Gilbert <kevin@kegisiteweb.com>
2022-01-13 13:40:21 -0500

    Added MUI doc and demo for : FileInput

---
+++ src/demo/mui/FileInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import FileInput from '@concrete-form/mui/FileInput'
+
+const Demo = () => (
+  <Form>
+    <FileInput name="demo" />
+  </Form>
+)
+
+export default Demo
