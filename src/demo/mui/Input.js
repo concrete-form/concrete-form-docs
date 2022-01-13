b15888b2c936c08a9f80b335f36c8df284004a89
Kevin Gilbert <kevin@kegisiteweb.com>
2022-01-13 13:39:39 -0500

    Added Input doc + examples

---
+++ src/demo/mui/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '@concrete-form/react-hook-form'
+import Input from '@concrete-form/mui/Input'
+
+const Demo = () => (
+  <Form>
+    <Input name="demo" label="I'm an input" />
+  </Form>
+)
+
+export default Demo
